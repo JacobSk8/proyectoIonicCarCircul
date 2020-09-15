ff78234ba42b291cd557cd4aecfe158525aa8527
Jacob Perez <jacobperez@MacBook-Pro-de-Jacob.local>
2020-09-15 14:06:35 -0500

    primer commitversion 1.0

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +7,69 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  constructor(public navCtrl : NavController) {}
 
-  constructor() {}
+  digitoUltimo: any;
+  circulan1: any;
+
+
+  
+
+  calcular(){
+    
+    var hoy = new Date();
+    var dd = hoy.getDate();
+    var mm = hoy.getMonth()+1;
+    var yyyy = hoy.getFullYear();
+    var num1 = this.digitoUltimo;
+
+
+
+    dd=this.addZero(dd);
+    mm=this.addZero(mm);
+
+    var circulan11 = " lunes, miercoles y viernes";
+    var circulan22 = " martes, jueves y sabado";
+    var error = " debe ingresar un valor";
+
+   if(num1 != ""){
+    if(mm%2){
+      if(num1%2){
+        this.circulan1 = circulan11;
+        
+      }else{
+        this.circulan1 = circulan22;
+
+      }
+  }else{
+      if(num1%2){
+        this.circulan1 = circulan22;
+
+      }else{
+        this.circulan1 = circulan11;
+
+    }
+  }
+}else{
+  this.circulan1 = error;
+}
+   
+    
+
+
+    let hoy2 = dd+'/'+mm+'/'+yyyy;
+    console.log(hoy2);
+
+  }
+
+
+  addZero(i) {
+    if (i < 10) {
+        i = '0' + i;
+    }
+    return i;
+}
+
 
 }
+
